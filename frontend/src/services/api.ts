4399c72988c3b9362355ca19d17856016e00f1e9
Ananya Venkatesh Babu <S4164413@student.rmit.edu.au>
2026-06-07 19:24:08 +1000

    Update the frontend baseURL

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -8,7 +8,7 @@ import { Unavailable } from "../types/unavail";
 //FSD Lectorial 8 code archive
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://a2-fsdpra01-07tue2-30pmveronikateam7.onrender.com/",
 });
 
 
